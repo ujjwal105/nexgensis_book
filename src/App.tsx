9c0767fef2f4885b457edb9eb8d2cfe3bca87d08
ujjwal105 <ujjwaltyagi2969@gmail.com>
2026-05-26 12:59:17 +0530

    fix shadcn installation issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import { Button } from "../components/ui/button";
 function App() {
   return (
     <div className="flex items-center justify-center h-screen text-5xl font-bold">
       Nexgensis Book Management App
+      <Button className="ml-4">Add Book</Button>
     </div>
   );
 }
